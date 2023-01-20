a8c2799f25c66bb1e06ef4b84d037942ce29ccc4
poormonitor <poormonitor@outlook.com>
2023-01-20 13:41:42 +0800

    add upgrade

--- views/src/views/System.vue
+++ views/src/views/System.vue
@@ -17,6 +17,14 @@ const CleanIsolateAction = () => {
         }
     });
 };
+
+const UpgradeAction = () => {
+    axios.post("/system/upgrade").then((response) => {
+        if (response.data.result === "success") {
+            message.success("更新请求成功。");
+        }
+    });
+};
 </script>
 
 <template>
@@ -26,9 +34,9 @@ const CleanIsolateAction = () => {
             清理系统中用户创建但没有完成上传的试卷。
             <template #action>
                 <div class="flex justify-end">
-                    <n-button type="primary" @click="CleanAction"
-                        >清理</n-button
-                    >
+                    <n-button type="primary" @click="CleanAction">
+                        清理
+                    </n-button>
                 </div>
             </template>
         </n-card>
@@ -36,9 +44,19 @@ const CleanIsolateAction = () => {
             清理系统中孤立的项目，包括考试、试卷和文件。
             <template #action>
                 <div class="flex justify-end">
-                    <n-button type="primary" @click="CleanIsolateAction"
-                        >清理</n-button
-                    >
+                    <n-button type="primary" @click="CleanIsolateAction">
+                        清理
+                    </n-button>
+                </div>
+            </template>
+        </n-card>
+        <n-card title="更新系统">
+            更新MyExam程序文件，并重新构建前端应用。后端需要手动重启。
+            <template #action>
+                <div class="flex justify-end">
+                    <n-button type="primary" @click="UpgradeAction">
+                        更新
+                    </n-button>
                 </div>
             </template>
         </n-card>
