e617f1081ebf216654d66f0199b2dcfd1e879c12
poormonitor <poormonitor@outlook.com>
2023-01-21 10:32:39 +0800

    fix mysql

--- models/__init__.py
+++ models/__init__.py
@@ -4,9 +4,11 @@ from sqlalchemy.orm import sessionmaker
 
 from config import get_config
 
-DATABASE_URL = get_config("DB_PATH")
+DATABASE_URL: str = get_config("DB_PATH")
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+args = {"check_same_thread": False} if DATABASE_URL.startswith("sqlite") else {}
+
+engine = create_engine(DATABASE_URL, connect_args=args)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
