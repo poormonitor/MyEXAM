d7ec057845e284dd66c3f3881f30f4fad45fe96d
poormonitor <poormonitor@outlook.com>
2023-01-26 19:40:46 +0800

    fix mysql

--- models/__init__.py
+++ models/__init__.py
@@ -6,9 +6,10 @@ from config import get_config
 
 DATABASE_URL: str = get_config("DB_PATH")
 
-args = {"check_same_thread": False} if DATABASE_URL.startswith("sqlite") else {}
+c_args = {"check_same_thread": False} if DATABASE_URL.startswith("sqlite") else {}
+e_args = {"pool_recycle": 3600} if "mysql" in DATABASE_URL else {}
 
-engine = create_engine(DATABASE_URL, connect_args=args)
+engine = create_engine(DATABASE_URL, connect_args=c_args, **e_args)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
